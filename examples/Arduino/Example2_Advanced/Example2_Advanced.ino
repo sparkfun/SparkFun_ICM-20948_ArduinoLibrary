5b85a7a2d249d6d3e161014962941523c6f6d15d
Bascy <basschouten65@gmail.com>
2021-05-24 12:39:38 +0200

    Moved loop() to bottom of Advanced example
    Fix method not known compiler errors

--- examples/Arduino/Example2_Advanced/Example2_Advanced.ino
+++ examples/Arduino/Example2_Advanced/Example2_Advanced.ino
@@ -169,22 +169,6 @@ void setup()
   SERIAL_PORT.println(F("Configuration complete!"));
 }
 
-void loop()
-{
-
-  if (myICM.dataReady())
-  {
-    myICM.getAGMT();              // The values are only updated when you call 'getAGMT'
-    //printRawAGMT( myICM.agmt ); // Uncomment this to see the raw values, taken directly from the agmt structure
-    printScaledAGMT(&myICM);      // This function takes into account the scale settings from when the measurement was made to calculate the values with units
-    delay(30);
-  }
-  else
-  {
-    SERIAL_PORT.println("Waiting for data");
-    delay(500);
-  }
-}
 
 // Below here are some helper functions to print the data nicely!
 
@@ -329,4 +313,22 @@ void printScaledAGMT(ICM_20948_I2C *sensor)
   printFormattedFloat(sensor->temp(), 5, 2);
   SERIAL_PORT.print(" ]");
   SERIAL_PORT.println();
+}
+
+  
+void loop()
+{
+
+  if (myICM.dataReady())
+  {
+    myICM.getAGMT();              // The values are only updated when you call 'getAGMT'
+    //printRawAGMT( myICM.agmt ); // Uncomment this to see the raw values, taken directly from the agmt structure
+    printScaledAGMT(&myICM);      // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+    delay(30);
+  }
+  else
+  {
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
 }
