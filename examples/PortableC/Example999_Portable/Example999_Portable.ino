5ee49bcdf155a439f1184d2fcda1f6ffe51f9af7
PaulZC <pc235603@gmail.com>
2021-07-26 08:33:07 +0100

    Update Example999_Portable.ino

--- examples/PortableC/Example999_Portable/Example999_Portable.ino
+++ examples/PortableC/Example999_Portable/Example999_Portable.ino
@@ -205,7 +205,6 @@ ICM_20948_Status_e my_read_i2c(uint8_t reg, uint8_t *buff, uint32_t len, void *u
       buff[i] = WIRE_PORT.read();
     }
   }
-  WIRE_PORT.endTransmission();
 
   return ICM_20948_Stat_Ok;
 }
